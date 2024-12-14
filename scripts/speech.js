ca5c6e83c708a5ca1f3c3dc4e075603c4c49304e
mperumal-usd <mperumal@sandiego.edu>
2024-12-13 17:54:44 -0800

    test coversation

--- scripts/speech.js
+++ scripts/speech.js
@@ -36,9 +36,9 @@ async function speakApi(text,audioPlayerElement) {
 async function getWorkSheet(number,topic){
   let query=""
   if(number)
-    query+=number+"&";
+    query+="number="+number+"&";
   if(topic)
-    query+=topic+"&";
+    query+="topic="+topic+"&";
 
   const apiUrl ='https://infinite-sands-52519-06605f47cb30.herokuapp.com/work_sheet'+ (query.length > 0 ? "?"+query :"");
    // Fetch the json
